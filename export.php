8572d3e1c862c1fc6e60c29763045d64fcb266d0
redivv <j.rajca45@gmail.com>
2020-03-15 11:57:01 +0100

    small export safe lock

--- export.php
+++ export.php
@@ -1,37 +1,42 @@
 <?php
-    $ironName = $_POST['ironName'];
-    $partsArray = $_POST['partsArray'];
-    $otherParts = array_slice($partsArray,3);
+    if(isset($_POST['partsArray'])) {
+        $ironName = $_POST['ironName'];
+        $partsArray = $_POST['partsArray'];
+        $otherParts = array_slice($partsArray,3);
+    
+        $fileName = generateRandomString();
 
-   $fileName = generateRandomString();
+        $configFile = fopen("txt/".$fileName.".txt","w");
 
-   $configFile = fopen("txt/".$fileName.".txt","w");
+        fwrite($configFile, pack("CCC",0xef,0xbb,0xbf)); 
 
-   fwrite($configFile, pack("CCC",0xef,0xbb,0xbf)); 
+        $line = $ironName."\n";
+        fwrite($configFile,$line);
 
-   $line = $ironName."\n";
-   fwrite($configFile,$line);
+        $line = "\tProcesor:\t\t\t".$partsArray[0]."\n";
+        fwrite($configFile,$line);
 
-   $line = "\tProcesor:\t\t\t".$partsArray[0]."\n";
-   fwrite($configFile,$line);
+        $line = "\tKarta Graficzna:\t\t".$partsArray[1]."\n";
+        fwrite($configFile,$line);
 
-   $line = "\tKarta Graficzna:\t\t".$partsArray[1]."\n";
-   fwrite($configFile,$line);
+        $line = "\tPamięć Ram:\t\t\t".$partsArray[2]."\n\n";
+        fwrite($configFile,$line);
 
-   $line = "\tPamięć Ram:\t\t\t".$partsArray[2]."\n\n";
-   fwrite($configFile,$line);
+        $line = "\tPamięć i Dodatki:\t\t";
+        fwrite($configFile,$line);
 
-   $line = "\tPamięć i Dodatki:\t\t";
-   fwrite($configFile,$line);
-
-   foreach ($otherParts as $part) {
-       $line = $part."\n\t\t\t\t\t";
-       fwrite($configFile,$line);
-   }
-   fclose($configFile);
-   echo $fileName;
-   exit;
+        foreach ($otherParts as $part) {
+            $line = $part."\n\t\t\t\t\t";
+            fwrite($configFile,$line);
+        }
+        fclose($configFile);
+        echo $fileName;
+        exit;
+    }else{
+        header('Location:http://iron.com.pl/konfigurator/');
+    }
 
     function generateRandomString($length = 10) {
         return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
     }
+    
